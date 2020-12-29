929e2c7161968233b6966e62ca37bad1f7df81d5
pallavg83 <33506404+pallavg83@users.noreply.github.com>
2020-12-29 16:49:30 +0530

    Update migctlversion.sh

--- migctlversion.sh
+++ migctlversion.sh
@@ -1,3 +1,3 @@
 gcloud container clusters get-credentials ansible-demo-cluster --zone us-central1-c --project acn-hybridcloudapplications
 migctl version
-migctl source create ce my-ce-src --project acn-hybridcloudapplications --json-key=m4a-ce-src.json
+migctl source create ce my-ce-src --project acn-hybridcloudapplications
