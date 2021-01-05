75a0743d44201e5ea49b7b75cfb5b593f20796db
pallavg83 <33506404+pallavg83@users.noreply.github.com>
2021-01-05 11:48:10 +0530

    Update migctlversion.sh

--- migctlversion.sh
+++ migctlversion.sh
@@ -1,3 +1,5 @@
 gcloud config set project acn-hybridcloudapplications
 gcloud container clusters get-credentials pr-migration-cluster --zone us-east1-b --project acn-hybridcloudapplications
 migctl version
+gcloud config set project acn-hybridcloudapplications
+migctl source create ce my-ce-src-5jan --project acn-hybridcloudapplications
