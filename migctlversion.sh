4fe6b6d98ab3c87c59b4e1cd59ef18943805291a
pallavg83 <33506404+pallavg83@users.noreply.github.com>
2021-01-05 19:53:16 +0530

    Update migctlversion.sh

--- migctlversion.sh
+++ migctlversion.sh
@@ -1,3 +1,3 @@
 gcloud config set project acn-hybridcloudapplications
 gcloud container clusters get-credentials pr-migration-cluster-1 --zone us-east1-b --project acn-hybridcloudapplications
-migctl migration create my-migration-1 --source my-ce-src-5jan1 --vm-id webapp-source-vm --intent Image
+migctl migration generate-artifacts my-migration-1
