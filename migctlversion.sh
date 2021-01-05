c219819c2da2226d6e91f64d5c8cfb146ece95a4
pallavg83 <33506404+pallavg83@users.noreply.github.com>
2021-01-05 19:24:42 +0530

    Update migctlversion.sh

--- migctlversion.sh
+++ migctlversion.sh
@@ -1,5 +1,3 @@
 gcloud config set project acn-hybridcloudapplications
-gcloud container clusters get-credentials pr-migration-cluster-1 --zone us-east1-b --project acn-hybridcloudapplications
-migctl version
-kubectl get SourceProvider
-migctl source create ce my-ce-src-5jan1 --project acn-hybridcloudapplications --json-key=/home/pallav_gupta/m4a-ce-src.json
+gcloud container clusters get-credentials pr-migration-cluster --zone us-east1-b --project acn-hybridcloudapplications
+migctl migration create my-migration --source my-ce-src-5jan1 --vm-id webapp-source-vm --intent Image
